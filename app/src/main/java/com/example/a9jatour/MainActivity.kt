783a43a87691c940f83e6768c7eb5e2df6c27a75
Administrator <godxuche@gmail.com>
2021-10-07 08:04:34 +0100

    referenced the Navigation View and the DrawerLayout in the Activity...

--- app/src/main/java/com/example/a9jatour/MainActivity.kt
+++ app/src/main/java/com/example/a9jatour/MainActivity.kt
@@ -3,24 +3,32 @@ package com.example.a9jatour
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.a9jatour.databinding.ActivityMainBinding
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
     private lateinit var toolbar: MaterialToolbar
     private lateinit var navController: NavController
+    private lateinit var navigationView: NavigationView
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
+        //Initialize Views
         toolbar = binding.appBarMain.toolbar
+        navigationView = binding.navView
+        drawerLayout = binding.drawerLayout
 
-        //get NavHostFragment and NavController
+        //Get NavHostFragment and NavController
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
         navController = navHostFragment.navController
     }
