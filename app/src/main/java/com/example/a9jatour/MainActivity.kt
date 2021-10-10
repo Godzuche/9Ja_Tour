8026992f0af4711ea76426eacf4091310ed8edd7
Administrator <godxuche@gmail.com>
2021-10-11 00:05:31 +0100

    replaced the drawerLayout and the NavigationView with the ConstraintLayout and a BottomNavigationView....

--- app/src/main/java/com/example/a9jatour/MainActivity.kt
+++ app/src/main/java/com/example/a9jatour/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.example.a9jatour.databinding.ActivityMainBinding
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -20,6 +21,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
     private lateinit var navigationView: NavigationView
     private lateinit var drawerLayout: DrawerLayout
+    private lateinit var bottomNavView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,27 +31,19 @@ class MainActivity : AppCompatActivity() {
 
         //Initialize Views
         toolbar = binding.appBarMain.toolbar
-        navigationView = binding.navView
-        drawerLayout = binding.drawerLayout
+        bottomNavView = binding.bottomNavView
 
         //Get NavHostFragment and NavController
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
         navController = navHostFragment.navController
 
-        //Define AppBarConfiguration: Connect DrawerLayout with Navigation Component
-        val appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+        //Define AppBarConfiguration
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
 
         //Connect Toolbar with NavController
         toolbar.setupWithNavController(navController, appBarConfiguration)
 
         //Connect NavigationView with NavController
-        navigationView.setupWithNavController(navController)
-    }
-
-    override fun onBackPressed() {
-        if (drawerLayout.isOpen)
-            drawerLayout.close()
-        else
-            super.onBackPressed()
+        bottomNavView.setupWithNavController(navController)
     }
 }
