3620a36a3f42d0015b58c89a112d7b22813db67e
Administrator <godxuche@gmail.com>
2021-10-11 03:36:10 +0100

    implemented the swipe to delete feature and added a Snackbar to undo delete...

--- app/src/main/java/com/example/a9jatour/ui/favorite/FavoriteListFragment.kt
+++ app/src/main/java/com/example/a9jatour/ui/favorite/FavoriteListFragment.kt
@@ -12,6 +12,7 @@ import com.example.a9jatour.R
 import com.example.a9jatour.data.manager.VacationSpots
 import com.example.a9jatour.data.manager.VacationSpots.favoriteCityList
 import com.example.a9jatour.data.model.City
+import com.google.android.material.snackbar.Snackbar
 import java.util.*
 
 class FavoriteFragment : Fragment() {
@@ -33,7 +34,7 @@ class FavoriteFragment : Fragment() {
     private fun setupRecyclerView(view: View?) {
         val context = requireContext()
         favoriteCityList = VacationSpots.favoriteCityList
-        view?.let { recyclerView = view.findViewById<RecyclerView>(R.id.favorite_recycler_view) }
+        view?.let { recyclerView = view.findViewById(R.id.favorite_recycler_view) }
 
         val layoutManager = LinearLayoutManager(context)
         layoutManager.orientation = RecyclerView.VERTICAL
@@ -47,7 +48,7 @@ class FavoriteFragment : Fragment() {
     }
 
     private val itemTouchHelper = ItemTouchHelper(object: ItemTouchHelper.SimpleCallback(
-        ItemTouchHelper.UP or ItemTouchHelper.DOWN, 0) {
+        ItemTouchHelper.UP or ItemTouchHelper.DOWN, ItemTouchHelper.RIGHT) {
         override fun onMove(
             recyclerView: RecyclerView,
             viewHolder: RecyclerView.ViewHolder,
@@ -61,7 +62,37 @@ class FavoriteFragment : Fragment() {
         }
 
         override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+            //Called when the item is swiped
+            val position = viewHolder.adapterPosition
+            val deletedCity = favoriteCityList[position]
 
+            deleteItem(position)
+            updateCityList(deletedCity, false)
+
+            Snackbar.make(recyclerView, "Deleted", Snackbar.LENGTH_LONG)
+                .setAction("UNDO") {
+                    undoDelete(position, deletedCity)
+                    updateCityList(deletedCity, true)
+                }
+                .show()
         }
     })
+
+    private fun deleteItem(position: Int) {
+        favoriteCityList.removeAt(position)
+        favoriteAdapter.notifyItemRemoved(position)
+        favoriteAdapter.notifyItemRangeChanged(position, favoriteCityList.size)
+    }
+
+    private fun updateCityList(deletedCity: City, isFavorite: Boolean) {
+        val cityList = VacationSpots.cityList!!
+        val position = cityList.indexOf(deletedCity)
+        cityList[position].isFavorite = isFavorite
+    }
+
+    private fun undoDelete(position: Int, deletedCity: City) {
+        favoriteCityList.add(position, deletedCity)
+        favoriteAdapter.notifyItemInserted(position)
+        favoriteAdapter.notifyItemRangeChanged(position, favoriteCityList.size)
+    }
 }
