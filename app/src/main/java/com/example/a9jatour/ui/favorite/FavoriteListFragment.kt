36eeedd573a738e32498583ec1b0796dd978a708
Administrator <godxuche@gmail.com>
2021-10-11 02:03:15 +0100

    implemented drag to arrange feature on the item in the RecyclerView...

--- app/src/main/java/com/example/a9jatour/ui/favorite/FavoriteListFragment.kt
+++ app/src/main/java/com/example/a9jatour/ui/favorite/FavoriteListFragment.kt
@@ -5,13 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.a9jatour.R
 import com.example.a9jatour.data.manager.VacationSpots
-
+import com.example.a9jatour.data.manager.VacationSpots.favoriteCityList
+import com.example.a9jatour.data.model.City
+import java.util.*
 
 class FavoriteFragment : Fragment() {
+
+    private lateinit var favoriteAdapter: FavoriteAdapter
+    private lateinit var favoriteCityList: MutableList<City>
+    private lateinit var recyclerView: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,15 +32,36 @@ class FavoriteFragment : Fragment() {
 
     private fun setupRecyclerView(view: View?) {
         val context = requireContext()
-        val recyclerView = view?.findViewById<RecyclerView>(R.id.favorite_recycler_view)
+        favoriteCityList = VacationSpots.favoriteCityList
+        view?.let { recyclerView = view.findViewById<RecyclerView>(R.id.favorite_recycler_view) }
 
         val layoutManager = LinearLayoutManager(context)
         layoutManager.orientation = RecyclerView.VERTICAL
-        recyclerView?.layoutManager = layoutManager
+        recyclerView.layoutManager = layoutManager
 
-        val favoriteAdapter = FavoriteAdapter(context, VacationSpots.favoriteCityList)
-        recyclerView?.adapter = favoriteAdapter
-        recyclerView?.setHasFixedSize(true)
+        favoriteAdapter = FavoriteAdapter(context, favoriteCityList)
+        recyclerView.adapter = favoriteAdapter
+        recyclerView.setHasFixedSize(true)
+
+        itemTouchHelper.attachToRecyclerView(recyclerView)
     }
 
+    private val itemTouchHelper = ItemTouchHelper(object: ItemTouchHelper.SimpleCallback(
+        ItemTouchHelper.UP or ItemTouchHelper.DOWN, 0) {
+        override fun onMove(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder,
+            targetViewHolder: RecyclerView.ViewHolder,
+        ): Boolean {
+            val fromPosition = viewHolder.adapterPosition
+            val toPosition = targetViewHolder.adapterPosition
+            Collections.swap(favoriteCityList, fromPosition, toPosition)
+            recyclerView.adapter?.notifyItemMoved(fromPosition, toPosition)
+            return true
+        }
+
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+
+        }
+    })
 }
